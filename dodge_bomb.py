2f1dd4e4e29ce8ce1b7f7ab810841e9346507266
raise <C0A24048ba@edu.teu.ac.jp>
2025-06-17 14:52:59 +0900

    練習4: 爆弾衝突

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,15 @@ DELTA ={ #移動用辞書
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.rect) -> tuple[bool,bool]:
+    yoko,tate =True,True
+    if rct.left <0 or WIDTH < rct.right:
+        yoko=False
+    if rct.top <0 or HEIGHT < rct.bottom:
+        tate=False
+    return yoko,tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -36,6 +45,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -53,8 +65,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
+        yoko,tate= check_bound(bb_rct)
+        if not yoko:
+            vx*=-1
+        if not tate:
+            vy*=-1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
